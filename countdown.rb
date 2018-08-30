87bbdd674e9710b2c67996920450e86ebe07b329
cd <coraldarling@outlook.com>
2018-08-30 21:02:24 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while 0 < n
+    puts "#{n} SECOND(S)!"
+    n -= 1  
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(i)
+  while 0 < i
+    sleep(1)
+    puts "#{i} SECOND(S)"
+    i -= 1 
+  end
+  "HAPPY NEW YEAR!"
+end  
